b780b6f0015b47b3829b9e442d4dc29f05077608
Nicolas SIX <nsix@georgiatech-metz.fr>
2018-02-12 11:57:54 +0100

    add script to generate dataset from csv

---
+++ generate_data_set.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [[ "$#" != "1" ]] ; then
+    echo "wrong number of argument"
+    echo "expected call is:"
+    echo "$0 output_directory"
+    exit 1
+fi
+
+input_file="pair.csv"
+out_dir=$1
+
+echo "creating dataset into $out_dir"
+
+while IFS=',' read -r img1 img2 dataset
+do
+    # remove space
+    dataset=${dataset:1}
+    img2=${img2:1}
+    if [[ "$dataset" != "0" ]] ; then
+        if [ ! -d "${out_dir}/${dataset}" ] ; then
+            mkdir "${out_dir}/${dataset}"
+        fi
+        img1_file_name=${img1##*/}
+        img1_file_name=${img1_file_name%.*}
+        img2_file_name=${img2##*/}
+        img2_file_name=${img2_file_name%.*}
+        img1_date=${img1%/*/*}
+        img1_date=${img1_date##*/}
+        img2_date=${img2%/*/*}
+        img2_date=${img2_date##*/}
+        dir_name="${img1_date}-${img1_file_name}_${img2_date}-${img2_file_name}"
+
+        if [ ! -d "${out_dir}/${dataset}/${dir_name}" ] ; then
+            mkdir "${out_dir}/${dataset}/${dir_name}"
+
+            # copy and resize images (keeping aspect ratio, zero will be added at load time)
+            convert "$img1" -resize 400x300 "${out_dir}/${dataset}/${dir_name}/image1.png" &
+            convert "$img2" -resize 400x300 "${out_dir}/${dataset}/${dir_name}/image2.png" &
+
+            # create empty masks
+            convert -resize 400x300\! xc:white "${out_dir}/${dataset}/${dir_name}/mask1.png" &
+            convert -resize 400x300\! xc:white "${out_dir}/${dataset}/${dir_name}/mask2.png" &
+
+            echo "Image1,Image2
+            $img1,$img2" > "${out_dir}/${dataset}/${dir_name}/pair.txt" &
+        else
+            echo "directory with name: $dir_name already exist"
+        fi
+    fi
+done < "$input_file"
