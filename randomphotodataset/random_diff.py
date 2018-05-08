5250a23e6a84c0f4b84dac35f2e16e45adee377a
Anliec <nicolas.six@pau.fr>
2018-05-08 13:25:30 +0200

    add messages for user

--- randomphotodataset/random_diff.py
+++ randomphotodataset/random_diff.py
@@ -8,11 +8,14 @@ from datasetgenerator.load_and_split_laser_pair import get_gps_coord
 
 
 def get_random_pair(count: int, dataset_path: str, min_offset: float=1.0):
+    print("Listing available pictures in " + dataset_path)
     photo_list = glob.glob(os.path.join(dataset_path, "VBags/[0-9]*/[0-9]*/[0-9]*.jpg"))
+    print("Done")
     if len(photo_list) == 0:
         raise RuntimeError("No images available in: {}\n"
                            "are you sure that the path to the 'lake' folder is correct?".format(dataset_path))
     pair_list = []
+    print("Generating pairs")
     while len(pair_list) < count:
         path1 = photo_list[random.randint(0, len(photo_list) - 1)]
         path2 = photo_list[random.randint(0, len(photo_list) - 1)]
@@ -33,19 +36,22 @@ def get_random_pair(count: int, dataset_path: str, min_offset: float=1.0):
         name = "{}_{}_{}_{}_{}_{}_{}_{}.jpg".format(splited_path1[-3], seq1_int, x1, y1,
                                                     splited_path2[-3], seq2_int, x2, y2)
         pair_list.append((path1, path2, name))
-
+    print("Done")
     return pair_list
         
 
 def generate_random_dataset(count: int, images_path: str, output_path: str, train_test_split: float=0.5):
     train_pair_count = int(count * train_test_split)
     pair_list = get_random_pair(count, images_path)
+    print("Writing train dataset")
     for i1, i2, name in pair_list[:train_pair_count]:
         shutil.copyfile(i1, os.path.join(output_path, "train/left/-1/", name))
         shutil.copyfile(i2, os.path.join(output_path, "train/right/-1/", name))
+    print("Writing test dataset")
     for i1, i2, name in pair_list[train_pair_count:]:
         shutil.copyfile(i1, os.path.join(output_path, "test/left/-1/", name))
         shutil.copyfile(i2, os.path.join(output_path, "test/right/-1/", name))
+    print("Done")
 
 
 def main():
